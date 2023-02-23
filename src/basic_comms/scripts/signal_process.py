a9128ee75f63506196cfd66d62646bbae07baa34
Manuel Ochoa <83720461+Manuelo247@users.noreply.github.com>
2023-02-22 23:57:23 -0600

    Fist Commit

---
+++ src/basic_comms/scripts/signal_process.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+from std_msgs.msg import Float32
+
+signal_data = 0
+
+def callTime(msg):
+    rospy.loginfo( " Time: %s", msg.data)
+
+def callSignal1(msg):
+    rospy.loginfo( " Sine: %s", msg.data)
+
+def callSignal(msg):
+    global signal_data
+    signal_data = msg.data
+    rospy.loginfo( " Sine_modified %s", signal_data)
+    
+if __name__ == '__main__':
+    rospy.init_node("process")
+    signal_processed = rospy.Subscriber("signal", Float32, callSignal)
+    pub = rospy.Publisher("signal_processed", Float32, queue_size = 10)
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        if signal_data < 0:
+            signal_data = signal_data*-1
+        signal_data += 10
+        signal_data/=2
+        pub.publish(signal_data)  
+        rate.sleep()
